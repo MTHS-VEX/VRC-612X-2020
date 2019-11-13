96da64b63bdc6118aadb0e0e0df995f2c5eecd2d
Henry222222 <57466405+Henry222222@users.noreply.github.com>
2019-11-13 14:26:59 -0800

    Write basic code

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,10 @@
 #include "main.h"
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
+auto chassis = ChassisControllerFactory::create(
+	{4,5,6},{-1,-2,-3},
+	AbstractMotor::gearset::green,
+	{4.125_in, 14.375_in}
+);
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -23,10 +13,7 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
 
-	pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -74,19 +61,12 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+	Controller master;
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
-
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
+		chassis.tank(
+			master.getAnalog(ControllerAnalog::leftY),
+			master.getAnalog(ControllerAnalog::rightY)
+		);
 	}
 }
